d5307df5cc5bf8ac58ebf19a31cbcaffbaa2069b
kachilala <kachilala23@gmail.com>
2021-02-01 17:54:12 +0100

    complete generic repository with unit of work

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,9 @@
+using HotelsLookUp.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,8 +28,10 @@ namespace HotelsLookUp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DatabaseContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("sqlConnection"))
+            );
 
-            services.AddControllers();
 
             services.AddCors(o => {
                 o.AddPolicy("CorsPolicy", builder => 
@@ -40,6 +44,9 @@ namespace HotelsLookUp
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "HotelsLookUp", Version = "v1" });
             });
+
+            services.AddControllers();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
